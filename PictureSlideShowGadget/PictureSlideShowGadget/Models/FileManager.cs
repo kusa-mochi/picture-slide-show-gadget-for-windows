fbe67016b1df30b56e4e8bcffa65107d1ce5068c
kusa-mochi <whoatemyapplepie@gmail.com>
2018-03-04 19:32:41 +0900

    initial commit.

---
+++ PictureSlideShowGadget/PictureSlideShowGadget/Models/FileManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSlideShowGadget.Models
+{
+    public class FileManager
+    {
+        private List<string> _filePathList = null;
+        private int _currentImageIndex = 0;
+
+        public FileManager(string dirPath, string[] extList = null)
+        {
+            if (string.IsNullOrEmpty(dirPath))
+            {
+                throw new ArgumentNullException("dirPath");
+            }
+
+            if (extList == null)
+            {
+                extList = new string[] { "jpg", "png", "bmp", "gif", "tiff" };
+            }
+
+            _filePathList = new List<string>();
+
+            foreach (string ext in extList)
+            {
+                _filePathList.AddRange(System.IO.Directory.GetFiles(dirPath, "*." + ext, System.IO.SearchOption.AllDirectories));
+            }
+        }
+
+        public string GetImageFilePath()
+        {
+            string output = _filePathList[_currentImageIndex++];
+            _currentImageIndex %= _filePathList.Count;
+            return output;
+        }
+    }
+}
---
+++ PictureSlideShowGadget/PictureSlideShowGadget/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+using PictureSlideShowGadget.Models;
+
+namespace PictureSlideShowGadget.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private FileManager _fileManager = null;
+        private string _title = "Picture Slide Show Gadget for Windows";
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            _fileManager = new FileManager(@"C:\Users\R\Desktop\新しいフォルダー");
+            DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Background);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += new EventHandler(UpdateImage);
+            timer.Start();
+        }
+
+        private void UpdateImage(object sender, EventArgs e)
+        {
+            ImageData = new BitmapImage(new System.Uri(_fileManager.GetImageFilePath()));
+        }
+
+        private BitmapImage _ImageData;
+        public BitmapImage ImageData
+        {
+            get { return _ImageData; }
+            set { SetProperty(ref _ImageData, value); }
+        }
+    }
+}
